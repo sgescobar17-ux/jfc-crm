55c41ee83ed4032bf82fe67b652faf7f8a04d792
Santiago <sgescobar17@gmail.com>
2025-12-15 23:31:31 -0500

    red de network

--- Desktop/Reto/envs/dev/variables.tf
+++ Desktop/Reto/envs/dev/variables.tf
@@ -9,3 +9,9 @@ variable "project_name" {
   type        = string
   default     = "jfc"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
