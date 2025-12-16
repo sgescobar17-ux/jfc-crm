7756a9e9dc5506bbc7302c666b7e764c332c61e1
Santiago <sgescobar17@gmail.com>
2025-12-15 23:45:29 -0500

    variable name de alb

--- Desktop/Reto/envs/dev/main.tf
+++ Desktop/Reto/envs/dev/main.tf
@@ -1,6 +1,6 @@
 module "network" {
-  source      = "../../modules/network"
-  
+  source = "../../modules/network"
+
   environment = var.environment
   vpc_cidr    = var.vpc_cidr
   tags        = local.tags
